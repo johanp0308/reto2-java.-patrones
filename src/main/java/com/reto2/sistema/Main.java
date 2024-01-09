eec0183b61e2fdb0a9ea7650220f9bbd0f0892fe
johanp0308 <sebastian-johan1@hotmail.com>
2024-01-09 12:50:13 -0500

    feat: :sparkles: Add code test

--- src/main/java/com/reto2/sistema/Main.java
+++ src/main/java/com/reto2/sistema/Main.java
@@ -1,7 +1,26 @@
 package com.reto2.sistema;
 
+import com.reto2.sistema.enitities.ControlInflarojo;
+import com.reto2.sistema.enitities.PanelTV;
+import com.reto2.sistema.enitities.adapters.SmartphoneAdapter;
+import com.reto2.sistema.enitities.asbtracs.DispositivoManipulacionTV;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DispositivoManipulacionTV dispositivo1 = new PanelTV();
+        DispositivoManipulacionTV dispositivo2 = new ControlInflarojo();
+        DispositivoManipulacionTV dispositivo3 = new SmartphoneAdapter();
+
+        System.out.println("-----------Panel TV--------------");
+        dispositivo1.encender();
+        dispositivo1.apagar();
+        System.out.println("--------Control Inflarojo--------");
+        dispositivo2.encender();
+        dispositivo2.apagar();
+        System.out.println("----------Smarth Phone----------");
+        dispositivo3.encender();
+        dispositivo3.apagar();
+        System.out.println("---------------------------------");
+
     }
 }
